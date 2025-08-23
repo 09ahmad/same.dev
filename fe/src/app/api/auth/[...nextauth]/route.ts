16be38cc5b69b06981322981a340244648d253a3
09ahmad <shkhahmad64@gmail.com>
2025-08-24 03:06:10 +0530

    Added session and jwt callback in nextauth fe

--- fe/src/app/api/auth/[...nextauth]/route.ts
+++ fe/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import { prismaClient } from "@repo/db/client";
 
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
@@ -23,11 +24,42 @@ export const authOptions = {
   pages: {
     signIn: "/signin",
   },
-  callbacks: {
-    async redirect({ baseUrl }: any) {
+    callbacks: {
+    async redirect({ baseUrl }:any) {
       return baseUrl + "/";
     },
+    async signIn({ user }:any) {
+      if (!user.email) return false;
+      const existingUser = await prismaClient.user.findUnique({
+        where: { email: user.email },
+      });
+      if (!existingUser) {
+        await prismaClient.user.create({
+          data: {
+            email: user.email,
+            name: user.name || "",
+          },
+        });
+      }
+      return true;
+    },
+    async jwt({ token, user }:any) {
+      if (user) {
+        const dbUser = await prismaClient.user.findUnique({
+          where: { email: user.email },
+        });
+        user.token = dbUser?.id;
+      }
+      return token;
+    },
+    async session({ session, token }:any) {
+      if (token.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
   },
+
 };
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
